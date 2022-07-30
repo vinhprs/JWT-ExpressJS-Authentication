3d7326aa5504ac1ec107c189699b920db9cdc2b1
Vinh Nguyễn <vinhnguyen19052002@gmail.com>
2022-07-30 13:40:58 +0700

    update port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 const { requireAuth, checkUser } = require('./middleware/authMiddleware')
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb+srv://vinhprs:01234766458@cluster0.znnks5j.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
 
 // routes
